94370522d65fb74a94bbbc4a3861533ce174847f
Zhansezim <zhansezim2946@gmail.com>
2026-02-09 19:38:36 +0500

    Complete Practice 3: Python functions, lambda, classes, and inheritance examples

---
+++ Lab3/classes/class_definition.py
@@ -0,0 +1,17 @@
+#1
+class MyClass:
+  x = 5
+
+#2
+p1 = MyClass()
+print(p1.x)
+
+#3
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
+
---
+++ Lab3/lambda/lambda_with_map.py
@@ -0,0 +1,4 @@
+#1
+numbers = [1, 2, 3, 4, 5]
+doubled = list(map(lambda x: x * 2, numbers))
+print(doubled)
